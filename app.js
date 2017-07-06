706cb0e6c36c14a86f2f7d1d9d33109d37c4f254
Ahmad Nasikin <ahmadnasikin@Ahmads-MacBook-Pro.local>
2017-07-06 21:41:46 +0700

    Tugas Express

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict'
 /** EXPRESS FROM SCRATCH V.0
 ---------------------------
 Buatlah sebuah aplikasi sederhana menggunakan Express JS
@@ -43,3 +44,51 @@ Setelah itu, buatlah code untuk membaca file dari data.json
 sehingga routing yang tadinya menampilkan object yang dibuat sendiri,
 sekarang menampilkan data yang ada di-file data.json menggunakan view engine .ejs
 **/
+/*
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res) {
+  res.send('Welcome to Express My App Ahmad Nasikin')
+})
+
+app.get('/user', function(req, res) {
+  res.send({
+    user_name: 'Ahmad Nasikin',
+    password: 1234455,
+    email: 'hactiv8@hacktiv8.com'
+  })
+})
+
+app.get('/cities', function(req, res) {
+  res.send({
+    nama: 'Magelang',
+    province: 'Jawa Tengah'
+  })
+})
+
+app.listen(3000)
+*/
+
+var express = require('express')
+var app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+  res.render('main')
+})
+
+app.get('/users', function(req, res) {
+  let fs = require('fs')
+  let data = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+  res.render('users', {data_ambil: data})
+})
+
+app.get('/cities', function(req, res) {
+  let fs = require('fs')
+  let data = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+  res.render('cities', {data_ambil: data})
+})
+
+app.listen(3000)
