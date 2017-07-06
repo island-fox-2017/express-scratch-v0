7ba1c372fb6e9c9c0ce6f7c95a0049ca22be3752
Oscar Hermawan <email": null>
2017-07-06 17:18:54 +0700

    v0

--- app.js
+++ app.js
@@ -1,45 +1,73 @@
-/** EXPRESS FROM SCRATCH V.0
----------------------------
-Buatlah sebuah aplikasi sederhana menggunakan Express JS
-
-- Release 0
-Buatlah 3 routing yang memenuhi spesifikasi berikut ini :
-
-URL --> http://localhost:3000/
-menampilkan "Welcome to Express My App [Nama_Kalian]"
-
-URL --> http://localhost:3000/users
-menampilkan data users berupa object. di release ini object dibuat manual,
-silakan menentukan property dan value dari object tersebut asalkan relevan dengan user
-
-URL --> http://localhost:3000/cities
-menampilkan data cities berupa object.
-silakan menentukan property dan value dari object cities juga untuk release 0 ini.
+var express = require('express')
+var path = require('path')
+var fs = require('fs')
 
 
-- Release 1
-Buatlah file data.json yang isinya ada 2 object yaitu users dan cities.
-contoh :
-{
-  users: [{
-    username: 'hacktiv8',
-    password: 'hacktiv8',
-    email: 'hactiv8@hacktiv8.com'
-  }, {
-    username: 'johndoe',
-    password: '12345',
-    email: 'john@doe.com'
-  }],
-  cities: [{
-    name: 'Jakarta'
-    province: 'DKI Jakarta'
-  }, {
-    name: 'Bandung'
-    province: 'Jawa Barat'
-  }]
-}
+var app = express();
 
-Setelah itu, buatlah code untuk membaca file dari data.json
-sehingga routing yang tadinya menampilkan object yang dibuat sendiri,
-sekarang menampilkan data yang ada di-file data.json menggunakan view engine .ejs
-**/
+var data = fs.readFileSync('data.json', 'utf8');
+var obj = JSON.parse(data)
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+  res.send('Welcome to Express My App Tamastro')
+})
+
+app.get('/users', function (req, res) {
+  var id = {
+    username1: JSON.stringify(obj.users[0].username),
+    password1: JSON.stringify(obj.users[0].password),
+    email1: JSON.stringify(obj.users[0].email),
+    username2: JSON.stringify(obj.users[1].username),
+    password2: JSON.stringify(obj.users[1].password),
+    email2: JSON.stringify(obj.users[1].email)
+  }
+  res.render('users', {
+    data: id
+  })
+})
+
+app.get('/cities', function (req, res) {
+  var city = {
+    name1: JSON.stringify(obj.cities[0].name),
+    provinsi1: JSON.stringify(obj.cities[0].province),
+    name2: JSON.stringify(obj.cities[1].name),
+    provinsi2: JSON.stringify(obj.cities[1].province)
+  }
+  res.render('cities', {
+    kota: city
+  })
+})
+
+
+
+//release 0
+
+// app.get('/', function (req, res) {
+//   res.send('Welcome to Express My App Tamastro')
+// })
+//
+// app.get('/users', function (req, res) {
+//   var id = {
+//     username: 'tamastro',
+//     password: 'aselole',
+//     email: 'azegazeg@joss.com'
+//   }
+//   res.render('users', {
+//     username: id
+//   })
+// })
+//
+// app.get('/cities', function (req, res) {
+//   var city = {
+//     name: 'jakarta',
+//     provinsi: 'DKI Jakarta'
+//   }
+//   res.render('cities', {
+//     cities: city
+//   })
+// })
+
+app.listen(3000)
