144435b90ada8928929fb753ead6b95ab2f306a9
rahid <hidayat.85@gmail.com>
2017-07-06 19:48:23 +0700

    express-scratch selesai

--- app.js
+++ app.js
@@ -1,45 +1,39 @@
-/** EXPRESS FROM SCRATCH V.0
----------------------------
-Buatlah sebuah aplikasi sederhana menggunakan Express JS
+var express = require('express')
+var app = express()
 
-- Release 0
-Buatlah 3 routing yang memenuhi spesifikasi berikut ini :
+var fs = require('fs');
+var options = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
-URL --> http://localhost:3000/
-menampilkan "Welcome to Express My App [Nama_Kalian]"
+app.set('view engine', 'ejs');
 
-URL --> http://localhost:3000/users
-menampilkan data users berupa object. di release ini object dibuat manual,
-silakan menentukan property dan value dari object tersebut asalkan relevan dengan user
+app.get('/', function (req, res) {
+  res.send('Welcome to Express My App Rahmat Hidayat')
+})
 
-URL --> http://localhost:3000/cities
-menampilkan data cities berupa object.
-silakan menentukan property dan value dari object cities juga untuk release 0 ini.
+//Release 0
+// app.get('/users', function(req, res){
+//   var objUser = {
+//     username: 'Rahid',
+//     duties: ['study', 'coding']
+//   }
+//   res.send(JSON.stringify(objUser))
+// })
+//
+// app.get('/cities', function(req, res){
+//   var city = {
+//     name: 'Jakarta',
+//     post_code: '12345'
+//   }
+//   res.send(JSON.stringify(city))
+// })
 
+//Release 1
+app.get('/users', function (req, res) {
+  res.render('users', {pesan: JSON.stringify(options['users'])})
+})
 
-- Release 1
-Buatlah file data.json yang isinya ada 2 object yaitu users dan cities.
-contoh :
-{
-  users: [{
-    username: 'hacktiv8',
-    password: 'hacktiv8',
-    email: 'hactiv8@hacktiv8.com'
-  }, {
-    username: 'johndoe',
-    password: '12345',
-    email: 'john@doe.com'
-  }],
-  cities: [{
-    name: 'Jakarta'
-    province: 'DKI Jakarta'
-  }, {
-    name: 'Bandung'
-    province: 'Jawa Barat'
-  }]
-}
+app.get('/cities', function (req, res) {
+  res.render('cities', {pesan: JSON.stringify(options['cities'])})
+})
 
-Setelah itu, buatlah code untuk membaca file dari data.json
-sehingga routing yang tadinya menampilkan object yang dibuat sendiri,
-sekarang menampilkan data yang ada di-file data.json menggunakan view engine .ejs
-**/
+app.listen(3000)
