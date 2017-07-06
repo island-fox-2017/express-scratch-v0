56b8b2d51f3fe74320e8c12b17f356ad4c987e61
Teja harsoni <teza.harsony230394@gmail.com>
2017-07-06 19:33:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,35 @@
-/** EXPRESS FROM SCRATCH V.0
----------------------------
-Buatlah sebuah aplikasi sederhana menggunakan Express JS
+var express = require ('express')
+var path = require('path');
+var app = express()
+var fs = require ('fs')
+var data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
+app.set('view engine', 'ejs');
 
-- Release 0
-Buatlah 3 routing yang memenuhi spesifikasi berikut ini :
+app.get('/',function (req, res) {
+  res.send ('Welcome to Express My App Teja Harsoni')
+})
 
-URL --> http://localhost:3000/
-menampilkan "Welcome to Express My App [Nama_Kalian]"
+app.get('/users', function (req, res) {
+  // RELEASE 0
+  // var obj = {"Nama" : "Teja Harsoni", "Lahir" : "Sumedang"}
+  //
+  //
+  // res.render('users', {obj_user: obj})
+  //
 
-URL --> http://localhost:3000/users
-menampilkan data users berupa object. di release ini object dibuat manual,
-silakan menentukan property dan value dari object tersebut asalkan relevan dengan user
+res.render('users',{data})
 
-URL --> http://localhost:3000/cities
-menampilkan data cities berupa object.
-silakan menentukan property dan value dari object cities juga untuk release 0 ini.
+})
+
+app.get('/cities', function (req, res){
+// RELEASE 0
+  // var obj = {"Kota":"Jakarta"}
+  //
+  // res.render('cities', {obj_kota: obj})
+
+res.render('cities',{data})
 
 
-- Release 1
-Buatlah file data.json yang isinya ada 2 object yaitu users dan cities.
-contoh :
-{
-  users: [{
-    username: 'hacktiv8',
-    password: 'hacktiv8',
-    email: 'hactiv8@hacktiv8.com'
-  }, {
-    username: 'johndoe',
-    password: '12345',
-    email: 'john@doe.com'
-  }],
-  cities: [{
-    name: 'Jakarta'
-    province: 'DKI Jakarta'
-  }, {
-    name: 'Bandung'
-    province: 'Jawa Barat'
-  }]
-}
+})
 
-Setelah itu, buatlah code untuk membaca file dari data.json
-sehingga routing yang tadinya menampilkan object yang dibuat sendiri,
-sekarang menampilkan data yang ada di-file data.json menggunakan view engine .ejs
-**/
+app.listen(3000)
