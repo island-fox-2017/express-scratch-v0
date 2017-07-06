5ca9ded1dbaf829d3076e4260a6045a55ea85027
pdvega <pdvegaa@gmail.com>
2017-07-06 19:09:24 +0700

    release-1 express scratch done

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 var app = express()
+var path = require('path')
+const fs = require('fs')
+// let data = require('./data.json')
+
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
   res.send('Welcome to Express My App PDVega')
 })
 
+/*release 0
 app.get('/users', function(req, res){
   var objUser = {
     user : 'pdvega',
@@ -20,52 +26,20 @@ app.get('/cities', function(req, res){
   }
   res.send(`${JSON.stringify(objCity, null, 2)}`)
 })
+*/
+
+app.locals.data = require('./data.json')
+
+//release 1
+app.get('/users', function(req, res){
+  res.render('users', {})
+})
+
+app.get('/cities', function(req, res){
+  let objData = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+  res.render('cities', {title:'cities', data : objData.cities})
+})
+
+
 
 app.listen(3000)
-
-/** EXPRESS FROM SCRATCH V.0
----------------------------
-Buatlah sebuah aplikasi sederhana menggunakan Express JS
-
-
-- Release 0
-Buatlah 3 routing yang memenuhi spesifikasi berikut ini :
-
-URL --> http://localhost:3000/
-menampilkan "Welcome to Express My App [Nama_Kalian]"
-
-URL --> http://localhost:3000/users
-menampilkan data users berupa object. di release ini object dibuat manual,
-silakan menentukan property dan value dari object tersebut asalkan relevan dengan user
-
-URL --> http://localhost:3000/cities
-menampilkan data cities berupa object.
-silakan menentukan property dan value dari object cities juga untuk release 0 ini.
-
-
-- Release 1
-Buatlah file data.json yang isinya ada 2 object yaitu users dan cities.
-contoh :
-{
-  users: [{
-    username: 'hacktiv8',
-    password: 'hacktiv8',
-    email: 'hactiv8@hacktiv8.com'
-  }, {
-    username: 'johndoe',
-    password: '12345',
-    email: 'john@doe.com'
-  }],
-  cities: [{
-    name: 'Jakarta'
-    province: 'DKI Jakarta'
-  }, {
-    name: 'Bandung'
-    province: 'Jawa Barat'
-  }]
-}
-
-Setelah itu, buatlah code untuk membaca file dari data.json
-sehingga routing yang tadinya menampilkan object yang dibuat sendiri,
-sekarang menampilkan data yang ada di-file data.json menggunakan view engine .ejs
-**/
